0e5c7ccdcf8856e7aea70a707dad58a7be0f0ace
lesflq <max.09seva@gmail.com>
2025-03-04 19:51:54 +0200

    realized player page, front-end for students is done

--- src/pages/student/MyEnrollments.jsx
+++ src/pages/student/MyEnrollments.jsx
@@ -1,9 +1,14 @@
 import React, { useContext, useState } from 'react'
 import { AppContext } from '../../context/AppContext'
+import {Line} from 'rc-progress'
+import Footer from '../../components/student/Footer'
+
 
 const MyEnrollments = () => {
 
-  const {enrolledCourses, calculateCourseDuration} = useContext(AppContext)
+  const {enrolledCourses, calculateCourseDuration, navigate} = useContext(AppContext)
+
+  const baseUrl = import.meta.env.VITE_BASE_URL || '';
 
   const [progressArray, setProgressArray] = useState([
     {lectureCompleted: 2, totalLectures: 4},
@@ -43,6 +48,8 @@ const MyEnrollments = () => {
               <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28' />
                 <div className='flex-1'>
                   <p className='mb-1 max-sm:text-sm'>{course.courseTitle}</p>
+                  <Line strokeWidth={2} percent={progressArray[index] ? (progressArray[index].lectureCompleted * 100) /
+                    progressArray[index].totalLectures : 0} className='bg-gray-300 rounded-full'></Line>
                 </div>
               </td>
               <td className='px-4 py-3 max-sm:hidden'>
@@ -52,13 +59,18 @@ const MyEnrollments = () => {
               {progressArray[index] && `${progressArray[index].lectureCompleted} / ${progressArray[index].totalLectures}`} <span>Lectures</span>
               </td>
               <td className='px-4 py-3 max-sm:text-right'>
-                <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>On Going</button>
+                <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'
+                 onClick={()=> navigate(`${baseUrl}/player/` + course._id)}>
+                  {progressArray[index] && progressArray[index].lectureCompleted / 
+                  progressArray[index].totalLectures === 1 ? 'Completed' : 'On Going'}
+                  </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
+    <Footer/>
     </>
     
   )
